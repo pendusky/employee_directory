129ec47138d81982e980698d4fa6023e4ef173d8
Pendusky Sampson <102203758+pendusky@users.noreply.github.com>
2022-05-31 22:41:50 +0100

    Add files via upload
    Employee directory with HTML , CSS and JS used

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const employee= document.querySelectorAll('.employee')
+
+const displayEmployees = (values) =>{
+    employee.forEach(element=> {
+        const name =element.children[1].innerHTML.toUpperCase();
+        const position =element.children[2].innerHTML.toUpperCase();
+        const job_type =element.children[3].innerHTML.toUpperCase();
+
+        if (name.includes(values) || position.includes(values) 
+        || job_type.includes(values)) {
+            element.getElementsByClassName.display="block"
+        }
+    })
+}
+
+searchEmployee.addEventListener('input', (e) =>{
+    displayEmployees(e.target.value.toUpperCase())
+})
